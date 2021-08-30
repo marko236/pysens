27baf3ff5d74b5419acbd0afc15cb8f0fcc91f0f
Marko Momčilović <markomomcilovic236@gmail.com>
2021-08-30 14:35:33 +0000

    Initial release

---
+++ sens.py
@@ -0,0 +1,6 @@
+import os
+import time
+while True:
+    os.system("clear") #optional, if you want to see only the latest output
+    os.system("sensors") #shouldn't modify this
+    time.sleep(1) #refresh rate in seconds
