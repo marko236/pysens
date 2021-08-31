2277b35b2157d9833c4de31b423324999a96d7cb
Marko Momčilović <89800283+marko236@users.noreply.github.com>
2021-08-31 14:50:28 +0200

    Update pysens.py

--- pysens.py
+++ pysens.py
@@ -2,5 +2,5 @@ import os
 import time
 while True:
     os.system("clear") #optional, if you want to see only the latest output
-    os.system("sensors") #shouldn't modify this
+    os.system("sensors")
     time.sleep(1) #refresh rate in seconds
