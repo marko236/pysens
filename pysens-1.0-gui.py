c18a39b374ded0ce7942eec160940fdeaebb0573
Marko Momčilović <89800283+marko236@users.noreply.github.com>
2021-09-03 22:30:20 +0200

    Initial release

---
+++ pysens-1.0-gui.py
@@ -0,0 +1,30 @@
+from tkinter.ttk import * # For 2.x use Tkinter instead of tkinter 
+from tkinter import END,Text,Tk # Same here
+from tkinter.messagebox import * # and here
+import os
+import time
+
+def exit():
+	ab=askyesno('PySens','Are you sure you want to quit?')
+	if ab==True:
+		quit()
+	else:
+		pass
+
+win=Tk()
+win.minsize(220,220)
+win.resizable(False,False)
+win.title("PySens - GUI")
+lb=Text(win,bg='black', fg='lime', font='Monospace 10',height=10,width=29)
+lb.configure(state='normal')
+lb.pack()
+
+def ins():
+	sn=os.popen('sensors')
+	sno=sn.read()
+	lb.delete('1.0',END)
+	lb.insert(END,sno)
+
+Button(win,text=">REFRESH<",command=ins).pack()
+Button(win,text=">>EXIT<<",command=exit).pack()
+win.mainloop()
